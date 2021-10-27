5918231a5aff6f924fc4961fedb8f2d97e22c0a3
Wikko Hieke <hieke@MySIMAGO.local>
2021-10-27 14:28:20 +0200

    Tests for Saldo are

--- src/test/java/com/chewie/repositories/SaldoRepositoryTest.java
+++ src/test/java/com/chewie/repositories/SaldoRepositoryTest.java
@@ -1,9 +1,7 @@
 package com.chewie.repositories;
 
 import com.chewie.domain.Saldo;
-import com.chewie.domain.User;
 import lombok.RequiredArgsConstructor;
-import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -17,39 +15,37 @@ import org.springframework.test.context.TestConstructor;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
+@DataJpaTest
+@RequiredArgsConstructor
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@DirtiesContext
+public class SaldoRepositoryTest {
+    @Autowired
+    private  SaldoRepository saldoRepository;
+    @Autowired
+    private TestEntityManager em;
 
+    private Long lastInsertId;
 
-    @DataJpaTest
-    @RequiredArgsConstructor
-    @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-    @DirtiesContext
-    public class SaldoRepositoryTest {
-        @Autowired
-        private  SaldoRepository saldoRepository;
-        @Autowired
-        private TestEntityManager em;
-
-        private Long lastInsertId;
-
-        @BeforeEach
-        void before() {
-            var saldo = new Saldo(1L);
-            saldo.setValue(0L);
-            lastInsertId=saldoRepository.save(saldo).getId();
-        }
-
-        @Test
-        @DisplayName("find Saldo by id")
-        void testFindById() {
-
-            var saldoFound = saldoRepository.findById(lastInsertId);
-            assertThat(saldoFound).isPresent();
-        }
-
-        @AfterEach
-        void after(){
-            saldoRepository.deleteById(lastInsertId);
-            lastInsertId=1L;
-        }
+    @BeforeEach
+    void before() {
+        var saldo = new Saldo(1L);
+        saldo.setValue(0L);
+        lastInsertId=saldoRepository.save(saldo).getId();
     }
+
+    @Test
+    @DisplayName("find Saldo by id")
+    void testFindById() {
+
+        var saldoFound = saldoRepository.findById(lastInsertId);
+        assertThat(saldoFound).isPresent();
+    }
+
+    @AfterEach
+    void after(){
+        saldoRepository.deleteById(lastInsertId);
+        lastInsertId=1L;
+    }
+}
 
